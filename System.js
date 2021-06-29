7321a272c43ae78d5aa7ef15a614d557ffc8631d
Endonian92 <83405953+Endonian92@users.noreply.github.com>
2021-06-29 14:31:58 +0900

    Update System.js

--- System.js
+++ System.js
@@ -1,9 +1,6 @@
 $("#page").keypress(function(){
         var str = event.keyCode;
-        setTimeout("link()", 0);
-        function link(){
-                location.href='main.html';
-        }
+        setTimeout("location.href='main.html';", 0);
 });
 function LoadMusic(){
         var music = new Audio('https://upload.wikimedia.org/wikipedia/commons/c/c3/Caro_Mio_Ben_-_Bryn_Williams.ogg');
