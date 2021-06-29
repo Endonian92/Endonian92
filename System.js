00183c7ab04f410f13eccce019da3ad7479a47aa
Endonian92 <83405953+Endonian92@users.noreply.github.com>
2021-06-29 14:19:53 +0900

    Update System.js

--- System.js
+++ System.js
@@ -1,1 +1,8 @@
-
+$("#page").keypress(function(){
+        var str = event.keyCode;
+        var music = new Audio('https://upload.wikimedia.org/wikipedia/commons/c/c3/Caro_Mio_Ben_-_Bryn_Williams.ogg');
+        if (str == 13){
+            music.play();
+            music.loop = true;
+        }
+  });
